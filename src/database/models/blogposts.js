4fa706b897bce8449c0de0c3ce89dce8191a7628
kodenart <mjuanvitorp@gmail.com>
2022-06-23 03:35:45 -0300

    feat(models): blogposts model created

---
+++ src/database/models/blogposts.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+     },
+    updated: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+     },
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'BlogPosts',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'users' });
+  };
+
+  return BlogPost;
+};
