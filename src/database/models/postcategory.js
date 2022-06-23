a92d6376e0685d37063aa757d6bf0d394a8f33c2
kodenart <mjuanvitorp@gmail.com>
2022-06-23 03:36:35 -0300

    feat(models): postcategory model created

---
+++ src/database/models/postcategory.js
@@ -0,0 +1,22 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER },
+    categoryId: { type: DataTypes.INTEGER },
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'PostsCategories',
+  });
+
+  PostCategory.associate = (models) => {
+
+    models.Category.belongsToMany(models.BlogPost,
+      { foreignKey: 'categoryId', as: 'categories', through: PostCategory });
+
+    models.BlogPost.belongsToMany(models.Category,
+      { foreignKey: 'postId', as: 'blogposts', through: PostCategory });
+  };
+
+  return PostCategory;
+};
