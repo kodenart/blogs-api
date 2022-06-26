03cb59daf3c4c0c6b5aecfcc1c8f97f2ce9f5e0b
kodenart <mjuanvitorp@gmail.com>
2022-06-25 23:06:12 -0300

    feat(api): adding new route and error middleware to the api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const errorHandler = require('./middlewares/errorHandler');
+const loginRoute = require('./routes/login');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', loginRoute);
+
+app.use(errorHandler);
 
 module.exports = app;
